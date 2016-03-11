b60f96fa4248631410cc14e51af27fb345bfc798
isometimesgit <isometimesgit@users.noreply.github.com>
2016-03-11 23:31:35 +0000

    Update index.js
    Added domain request blacklist 
    Fixed some minor bugs

--- index.js
+++ index.js
@@ -12,25 +12,27 @@ var plugin = ({
     button: null,
     // initialise plugin
     init: function() { 
-        this.exp.build();
-        config.on("uriExtensions", this.exp.build.bind(this.exp));
-        config.on("enableYoutube", this.exp.build.bind(this.exp));     
+        this.extensions.build();
+        this.blacklist.build();
+        config.on("uriExtensions", this.extensions.build.bind(this.extensions));
+        config.on("enableYoutube", this.extensions.build.bind(this.extensions));
+        config.on("enableDomainBlacklist", this.blacklist.build.bind(this.blacklist));
+        config.on("domainBlacklist", this.blacklist.build.bind(this.blacklist));
+        this.active = config.prefs['active'];
         this.button = ActionButton({
             id: "activatetoggle",
-            label: "Start Video Assistant",
+            label: this.active ? "Stop Video Assistant" : "Start Video Assistant",
             "icon": {
-                "16": "./icon-16.png",
-                "32": "./icon-16.png"     
+                "16": this.active ? "./icon-16-red.png" : "./icon-16.png",
+                "32": this.active ? "./icon-32-red.png" : "./icon-16.png"
             },
             onClick: this.onClickButton.bind(this)
         });
         this.shutdownObserver.observe = this.onShutdown.bind(this);
         this.requestObserver.observe = this.onRequest.bind(this);
         observerService.addObserver(this.shutdownObserver, "quit-application", false);  
-        this.active = config['active'];
         if (this.active)
             this.activate(true);
-        console.error('init');
         return this;       
     },
     // called when toolbar button clicked
@@ -39,10 +41,9 @@ var plugin = ({
         this.button.label = this.active ? "Stop Video Assistant" : "Start Video Assistant";
         this.button.icon = {
             "16": this.active ? "./icon-16-red.png" : "./icon-16.png",
-            "32": this.active ? "./icon-32-red.png" : "./icon-16.png"     
+            "32": this.active ? "./icon-32-red.png" : "./icon-16.png"
         }   
         this.activate(this.active);
-        console.error('clicked');
     },
     // called when plugin is shutdown
     onShutdown: function(subject, topic, data) {  
@@ -51,13 +52,15 @@ var plugin = ({
         }
         config.prefs["active"] = this.active;
         observerService.removeObserver(this.shutdownObserver, "quit-application");
-        console.error('shutdown');
     },
     // called when a request is made
     onRequest: function(subject, topic, data) {  
         subject.QueryInterface(Ci.nsIHttpChannel);
         var url = subject.URI.spec;
-        if (this.exp.test(url)) {      
+        if (config.prefs['enableDomainBlacklist'] && this.blacklist.test(url)) {            
+            return;
+        }
+        if (this.extensions.test(url)) {      
             if (config.prefs["cancelDefault"] || config.prefs["closeTab"]) {
                 subject.cancel(Cr.NS_BINDING_ABORTED);
             }                                         
@@ -66,7 +69,6 @@ var plugin = ({
                 tabs.activeTab.close();
             }
         }
-        console.error('onRequest:'+url);
     },
     // start / stop plugin listening to requests
     activate: function(active) {
@@ -88,22 +90,43 @@ var plugin = ({
             console.error("Error launching app("+config.prefs['executePath']+") with url("+url+"): "+ex.message);
         }
     },
-    // cache regular expression which tests urls with file extensions / hosts
-    exp: {
+    // url path extensions / youtube
+    extensions: {
         regex: null,
         build: function() {
             var ext = config.prefs["uriExtensions"].trim().replace(/,/g,"|");
-            if (!ext.length) {
-                return false;
-            }
-            if (config.prefs["enableYoutube"]) {
-                this.regex = new RegExp("^([^\?]+)[.]("+ext+")([\?#]+.*)*|https:\\/\\/www\\.youtube\\.com\\/watch\\?v=.+$", "i");
-            } else {
-                this.regex = new RegExp("^([^\?]+)[.]("+ext+")([\?#]+.*)*$", "i");
+            if (ext.length) {        
+                if (config.prefs["enableYoutube"]) {
+                    this.regex = new RegExp("^([^\?]+)[.]("+ext+")([\?#]+.*)*|https:\\/\\/www\\.youtube\\.com\\/watch\\?v=.+$", "i");
+                } else {
+                    this.regex = new RegExp("^([^\?]+)[.]("+ext+")([\?#]+.*)*$", "i");
+                }
             }
         },
         test: function(url) {
             return this.regex.test(url);       
+        }
+    },
+    // blacklisted domains
+    blacklist: {
+        list: null,
+        build: function() {
+           var domains = config.prefs["domainBlacklist"].trim();
+            if (domains.length) {
+                this.list = domains.split(/\s*,\s*/);
+            }
+        },
+        test: function(url) {
+            var i, len = this.list.length;
+            if (!len) {
+                return false;
+            }
+            for (i=0; i < len; i++) {
+                if (url.indexOf(this.list[i]) > -1) {
+                    break;
+                }
+            }     
+            return (i < len);   
         }
     }
 }.init());
